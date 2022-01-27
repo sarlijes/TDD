3e60d7c826bcf9ab61d70e3b805ae7f1f7a3174e
Jessica Sarlin <jessica.sarlin@gmail.com>
2022-01-27 14:25:36 +0200

    Fix typo in test

--- tdd-mooc-tetris/test/Utilstest.test.mjs
+++ tdd-mooc-tetris/test/Utilstest.test.mjs
@@ -9,7 +9,7 @@ describe("2-dimensional array to string", () => {
       [7, 8, 9],
     ];
     const result = twoDimensionalArraytoString(arr);
-    expect(result.toString()).to.equal("123\n456\n789");
+    expect(result.toString()).to.equal("123\n456\n789\n");
   });
 
   it("2-dimensional array of chars", () => {
@@ -18,6 +18,6 @@ describe("2-dimensional array to string", () => {
       ["4", "5", "6"],
     ];
     const result = twoDimensionalArraytoString(arr);
-    expect(result.toString()).to.equal("123\n456\n789");
+    expect(result.toString()).to.equal("123\n456\n");
   });
 });
