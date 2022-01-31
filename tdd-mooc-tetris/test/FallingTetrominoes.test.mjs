9477d2d605d94502ac9154ac76e8bec2f83273e2
Jessica Sarlin <jessica.sarlin@gmail.com>
2022-01-31 10:04:26 +0200

    Revert "WIP - for structure for looping all chars in a Tetromino"
    This reverts commit bce6b2bb5a2fe4ebcd5b687d4380a036bc15a6ff.

--- tdd-mooc-tetris/test/FallingTetrominoes.test.mjs
+++ tdd-mooc-tetris/test/FallingTetrominoes.test.mjs
@@ -11,7 +11,7 @@ function fallToBottom(board) {
 // TODO remove after everything works
 // eslint-disable-next-line no-unused-vars
 function printHelper(result) {
-  const splitted = result.toString().split("\n");
+  const splitted = result.split("\n");
 
   for (let row in splitted) {
     console.log(splitted[row]);
@@ -41,10 +41,8 @@ describe("T_SHAPE Tetromino: Falling tetrominoes", () => {
 
   xit("stop when they hit the bottom", () => {
     board.drop(Tetromino.T_SHAPE);
-    printHelper(board);
-
     fallToBottom(board);
-    printHelper(board);
+
     expect(board.toString()).to.equalShape(
       `..........
        ..........
