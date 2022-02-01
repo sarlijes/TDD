1b7e1821ba7a8f3c70fac02124de43cd6ccadd11
Jessica Sarlin <jessica.sarlin@gmail.com>
2022-02-01 12:42:12 +0200

    failing test for '
    a falling tetromino can be rotated left'

--- tdd-mooc-tetris/test/RotatingFallingTetrominoes.test.mjs
+++ tdd-mooc-tetris/test/RotatingFallingTetrominoes.test.mjs
@@ -6,18 +6,26 @@ import { Tetromino } from "../src/Tetromino.mjs";
 describe("Rotating falling tetrominoes", () => {
 
   let board;
+  let shape;
   beforeEach(() => {
     board = new Board(12, 6);
-    board.drop(Tetromino.T_SHAPE);
+    shape = Tetromino.T_SHAPE;
+    board.drop(shape);
   });
 
 
-  it("a falling tetromino can be moved left", () => {
-    board.moveLeft();
+  it("a falling tetromino can be rotated left", () => {
+    board.moveDown();
+    board.moveDown();
+    expect(shape.rotateLeft().toString()).to.equalShape(
+      `.T.
+       TT.
+       .T.`
+    );
     expect(board.toString()).to.equalShape(
-      `....T.......
-       ...TTT......
-       ............
+      `.....T......
+       ....TT......
+       .....T......
        ............
        ............
        ............`
@@ -44,5 +52,6 @@ describe("Rotating falling tetrominoes", () => {
 
 // - a falling tetromino can be rotated
 // - it cannot be rotated when there is no room to rotate
-// - [wall kick](https://tetris.fandom.com/wiki/Wall_kick): when it is up against a wall and is rotated, but there is no
+// - [wall kick](https://tetris.fandom.com/wiki/Wall_kick):
+//    when it is up against a wall and is rotated, but there is no
 //   room to rotate, move it away from the wall if possible
