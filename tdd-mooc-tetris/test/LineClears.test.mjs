0fe294d86f7ec7eca173765f54ef41e3779cad94
Jessica Sarlin <jessica.sarlin@gmail.com>
2022-02-03 16:00:43 +0200

    Add another test to line clears

--- tdd-mooc-tetris/test/LineClears.test.mjs
+++ tdd-mooc-tetris/test/LineClears.test.mjs
@@ -5,15 +5,13 @@ import { Tetromino } from "../src/Tetromino.mjs";
 import { fallToBottom } from "./TestHelpers.mjs";
 
 describe("Line clears", () => {
-
   let board;
 
   beforeEach(() => {
-    board = new Board(8, 3);
+    board = new Board(8, 4);
   });
 
   it("Two I shapes land at the bottom, the line clears", () => {
-    board = new Board(8, 3);
     board.drop(Tetromino.I_SHAPE);
     board.moveLeft();
     board.moveLeft();
@@ -29,14 +27,43 @@ describe("Line clears", () => {
     expect(board.toString()).to.equalShape(`
       ........
       ........
+      ........
       ........`
     );
-
   });
 
-  it("", () => {
+  it("Four O shapes land at the bottom, the line clears", () => {
+    board.drop(Tetromino.O_SHAPE);
+    board.moveLeft();
+    board.moveLeft();
+    board.moveLeft();
+    fallToBottom(board);
+
+    board.drop(Tetromino.O_SHAPE);
+    board.moveLeft();
+    fallToBottom(board);
+
+    board.drop(Tetromino.O_SHAPE);
+    board.moveRight();
+    board.moveRight();
+    board.moveRight();
+    fallToBottom(board);
+
+    board.drop(Tetromino.O_SHAPE);
+    board.moveRight();
+    fallToBottom(board);
+
+    board.checkClear();
+    expect(board.toString()).to.equalShape(`
+    ........
+    ........
+    ........
+    ........`
+    );
   });
 
-
+  // TODO add test for a case where the block should stop when
+  // dropped - maybe should check coordinates? Or simple check
+  // when drawing the shape - if some of the cells is already occupied
 
 });
