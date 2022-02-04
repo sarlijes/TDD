317321043502261b39c61d30d0cb9e70483c6b67
Jessica Sarlin <jessica.sarlin@gmail.com>
2022-02-04 11:00:23 +0200

    WIP - tests for wall kick

--- tdd-mooc-tetris/test/WallKick.test.mjs
+++ tdd-mooc-tetris/test/WallKick.test.mjs
@@ -14,17 +14,12 @@ import { Board } from "../src/Board.mjs";
 //   room to rotate, move it away from the wall if possible
 
 describe("Wall kicks", () => {
-  let board;
-  let shape;
-
-  beforeEach(() => {
-    board = undefined;
-    board = new Board(12, 6);
-    shape = Tetromino.T_SHAPE;
-    board.drop(shape);
-  });
 
   it("After rotating to right", () => {
+    let board = new Board(12, 6);
+    let shape = Tetromino.T_SHAPE;
+    board.drop(shape);
+
     board.rotateRight();
     expect(board.toString()).to.equalShape(`
       .....T......
@@ -37,6 +32,10 @@ describe("Wall kicks", () => {
   });
 
   it("After rotating right and moved to left egde", () => {
+    let board = new Board(12, 6);
+    let shape = Tetromino.T_SHAPE;
+    board.drop(shape);
+
     board.rotateRight();
     moveToLeftEdge(board);
     expect(board.toString()).to.equalShape(`
@@ -50,20 +49,23 @@ describe("Wall kicks", () => {
   });
 
 
-  // it("After rotating right, moved to left egde and rotated", () => {
-  //   board = new Board(12, 6);
-  //   board.rotateRight();
-  //   moveToLeftEdge(board);
-  //   board.rotateRight();
-  //   expect(board.toString()).to.equalShape(`
-  //      ............
-  //      TTT.........
-  //      .T..........
-  //      ............
-  //      ............
-  //      ............`
-  //   );
-  // });
+  it("After rotating right, moved to left egde and rotated", () => {
+    let board = new Board(12, 6);
+    let shape = Tetromino.T_SHAPE;
+    board.drop(shape);
+
+    board.rotateRight();
+    moveToLeftEdge(board);
+    board.rotateRight();
+    expect(board.toString()).to.equalShape(`
+       ............
+       TTT.........
+       .T..........
+       ............
+       ............
+       ............`
+    );
+  });
 
 
   it("", () => {
