0cd76468a95d69fdb7948630d45995f829514ef5
Jessica Sarlin <jessica.sarlin@gmail.com>
2022-02-04 18:44:24 +0200

    Implement date restriction to only include certain customers. Add mock data

--- tdd-mooc-untestables/src/CustomerRegister.mjs
+++ tdd-mooc-untestables/src/CustomerRegister.mjs
@@ -20,20 +20,52 @@ export class CustomerRegister {
         return;
       }
       let customers = [];
+      let todayString = this.getDayString();
+
       const splitted = data.split("\n");
       splitted.forEach(item => {
         const parts = item.split(";");
-        customers.push(new Customer(parts[1], parts[2], parts[3], parts[4], parts[5]));
+
+        if (parts[5] === this.getDayString()) {
+          customers.push(new Customer(
+            parts[1], parts[2], parts[3], parts[4], parts[5]
+          ));
+        }
+
       });
 
       console.log(customers);
-    });
+      console.log("Customer count: " + customers.length);
 
+
+    });
+    this.getDayString();
   }
 
+
+  getDayString() {
+    let days = new Date().getDate();
+    if (parseInt(days) < 10) {
+      days = "0" + days;
+    }
+    let month = parseInt(new Date().getMonth()) + 1;
+    if (parseInt(month) < 10) {
+      month = "0" + month;
+    }
+    let year = new Date().getFullYear();
+
+    return days + "/" + month + "/" + year;
+  }
 }
 
 // Global variables
 // File system
 // Time
 // Randomness
+
+// Parse purchase total to double
+// Only add customers above silver limit to list - adjust mock data if needed - see big file
+// Write result to file
+// Implement randomness - maybe a lottery ticket ID to all customers or something
+
+// Proceed to writing tests
