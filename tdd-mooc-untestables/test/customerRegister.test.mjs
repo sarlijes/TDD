4fac4baf67da81d0ff3524e546499c8f3381056b
Jessica Sarlin <jessica.sarlin@gmail.com>
2022-02-07 12:24:59 +0200

    Tests for random int

---
+++ tdd-mooc-untestables/test/customerRegister.test.mjs
@@ -0,0 +1,38 @@
+import { expect } from "chai";
+import { CustomerRegister } from "../src/CustomerRegister.mjs";
+
+describe("Example test fixture", () => {
+  it("Example test", () => {
+
+    const register = new CustomerRegister();
+
+    register.listGoldCustomersOfYesterday();
+    expect(3).to.equal(3);
+
+
+
+  });
+});
+
+describe("Helper function: get random integer", () => {
+
+  let register;
+
+  beforeEach(() => {
+    register = new CustomerRegister();
+  });
+
+  it("The min limit is respected", () => {
+    for (let x = 0; x < 1000; x++ ) {
+      const rand = register.getRandomInteger();
+      expect(rand).to.be.at.least(100000);
+    }
+  });
+
+  it("The max limit is respected", () => {
+    for (let x = 0; x < 1000; x++ ) {
+      const rand = register.getRandomInteger();
+      expect(rand).to.be.at.most(999998);
+    }
+  });
+});
