289d48a76a1557df36762bc661149f8615476b69
Jessica Sarlin <jessica.sarlin@gmail.com>
2022-02-08 17:05:20 +0200

    Line coverage to 100%

--- tdd-mooc-legacy-code/src/gilded_rose.js
+++ tdd-mooc-legacy-code/src/gilded_rose.js
@@ -13,13 +13,15 @@ class Shop {
 
   updateQuality() {
     for (var i = 0; i < this.items.length; i++) {
-      if (this.items[i].name !== "Aged Brie"
-      && this.items[i].name !== "Backstage passes to a TAFKAL80ETC concert") {
+      // eslint-disable-next-line max-len
+      if (this.items[i].name !== "Aged Brie" && this.items[i].name !== "Backstage passes to a TAFKAL80ETC concert") {
+        // Only "Sulfuras, Hand of Ragnaros" is handled here
         if (this.items[i].quality > 0) {
           if (this.items[i].name !== "Sulfuras, Hand of Ragnaros") {
             this.items[i].quality = this.items[i].quality - 1;
           }
         }
+        //
       } else {
         if (this.items[i].quality < 50) {
           this.items[i].quality = this.items[i].quality + 1;
--- tdd-mooc-legacy-code/test/test_gilded_rose.js
+++ tdd-mooc-legacy-code/test/test_gilded_rose.js
@@ -10,3 +10,177 @@ describe("Gilded Rose", function () {
     expect(items[0].name).to.equal("foo");
   });
 });
+
+
+describe("Aged Brie", function () {
+
+  let gildedRose;
+  let items;
+
+  beforeEach(() => {
+    gildedRose = new Shop([new Item("Aged Brie", 0, 0)]);
+    items = gildedRose.updateQuality();
+  });
+
+  it("the product is returned", function () {
+    expect(items[0].name).to.equal("Aged Brie");
+  });
+
+  describe("Quality is updated", function () {
+
+    it("When quality is 0, it's raised to 1", function () {
+      gildedRose = new Shop([new Item("Aged Brie", 7, 0)]);
+      items = gildedRose.updateQuality();
+      expect(items[0].quality).to.equal(1);
+    });
+
+    it("When quality is max, it stays at max", function () {
+      gildedRose = new Shop([new Item("Aged Brie", 7, 50)]);
+      items = gildedRose.updateQuality();
+      expect(items[0].quality).to.equal(50);
+    });
+  });
+
+  describe("SellIn is updated", function () {
+
+    it("When sellIn is 7", function () {
+      gildedRose = new Shop([new Item("Aged Brie", 7, 0)]);
+      items = gildedRose.updateQuality();
+      expect(items[0].sellIn).to.equal(6);
+    });
+
+    it("When sellIn is 0", function () {
+      gildedRose = new Shop([new Item("Aged Brie", 0, 0)]);
+      items = gildedRose.updateQuality();
+      expect(items[0].sellIn).to.equal(-1);
+    });
+  });
+
+});
+
+
+describe("Backstage passes to a TAFKAL80ETC concert", function () {
+  let gildedRose;
+  let items;
+  const passes = "Backstage passes to a TAFKAL80ETC concert";
+
+  it("the product is returned", function () {
+    gildedRose = new Shop([new Item(passes, 0, 0)]);
+    items = gildedRose.updateQuality();
+    expect(items[0].name).to.equal(passes);
+  });
+
+  describe("Quality is updated", function () {
+
+    it("When quality is 0, it goes to 2", function () {
+      gildedRose = new Shop([new Item(passes, 7, 0)]);
+      items = gildedRose.updateQuality();
+      expect(items[0].quality).to.equal(2);
+    });
+
+    it("When quality is max, it stays at max", function () {
+      gildedRose = new Shop([new Item(passes, 7, 50)]);
+      items = gildedRose.updateQuality();
+      expect(items[0].quality).to.equal(50);
+    });
+  });
+
+  describe("SellIn is updated", function () {
+
+    it("When sellIn is 7", function () {
+      gildedRose = new Shop([new Item(passes, 7, 0)]);
+      items = gildedRose.updateQuality();
+      expect(items[0].sellIn).to.equal(6);
+    });
+
+    it("When sellIn is 0", function () {
+      gildedRose = new Shop([new Item(passes, 0, 0)]);
+      items = gildedRose.updateQuality();
+      expect(items[0].sellIn).to.equal(-1);
+    });
+  });
+
+  it("quality goes to 0 after the concert has taken place", function () {
+    gildedRose = new Shop([new Item(passes, 1, 25)]);
+    items = gildedRose.updateQuality();
+    expect(items[0].sellIn).to.equal(0);
+  });
+
+});
+
+
+describe("Sulfuras, Hand of Ragnaros", function () {
+  let gildedRose;
+  let items;
+  const itemName = "Sulfuras, Hand of Ragnaros";
+
+  it("the product is returned", function () {
+    gildedRose = new Shop([new Item(itemName, 0, 0)]);
+    items = gildedRose.updateQuality();
+    expect(items[0].name).to.equal(itemName);
+  });
+
+  describe("Quality is updated", function () {
+
+    it("When quality is 0, stays in 0", function () {
+      gildedRose = new Shop([new Item(itemName, 7, 0)]);
+      items = gildedRose.updateQuality();
+      expect(items[0].quality).to.equal(0);
+    });
+
+    it("When quality is max, it stays at max", function () {
+      gildedRose = new Shop([new Item(itemName, 7, 50)]);
+      items = gildedRose.updateQuality();
+      expect(items[0].quality).to.equal(50);
+    });
+  });
+
+  describe("SellIn is updated", function () {
+
+    it("When sellIn is 7, it stays the same", function () {
+      gildedRose = new Shop([new Item(itemName, 7, 0)]);
+      items = gildedRose.updateQuality();
+      expect(items[0].sellIn).to.equal(7);
+    });
+
+    it("When sellIn is 0, it stays the same", function () {
+      gildedRose = new Shop([new Item(itemName, 0, 0)]);
+      items = gildedRose.updateQuality();
+      expect(items[0].sellIn).to.equal(0);
+    });
+  });
+
+});
+
+// Additional item to cover all rows
+describe("Monstera Albo Variegata", function () {
+
+  let gildedRose;
+  let items;
+  const itemName = "Monstera Albo Variegata";
+
+  it("the product is returned", function () {
+    gildedRose = new Shop([new Item(itemName, 0, 0)]);
+    items = gildedRose.updateQuality();
+    expect(items[0].name).to.equal(itemName);
+  });
+
+  it("When quality is 7, it goes to 6", function () {
+    gildedRose = new Shop([new Item(itemName, 7, 7)]);
+    items = gildedRose.updateQuality();
+    expect(items[0].quality).to.equal(6);
+  });
+
+  it("when item was supposed to be sold yesterday, quality goes down", function () {
+    gildedRose = new Shop([new Item(itemName, 0, 7)]);
+    items = gildedRose.updateQuality();
+    expect(items[0].quality).to.equal(5);
+  });
+
+});
+
+describe("", function () {
+  it("", function () {
+
+  });
+});
