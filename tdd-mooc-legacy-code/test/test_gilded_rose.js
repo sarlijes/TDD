8b145f256b8c14124c2133f67a98ba3ecd0e1b5c
Jessica Sarlin <jessica.sarlin@gmail.com>
2022-02-09 09:23:20 +0200

    cleanup

--- tdd-mooc-legacy-code/test/test_gilded_rose.js
+++ tdd-mooc-legacy-code/test/test_gilded_rose.js
@@ -100,32 +100,49 @@ describe("Aged Brie", function () {
 
   });
 
-
-
-
-
-
 });
-
 
 describe("Backstage passes to a TAFKAL80ETC concert", function () {
   let gildedRose;
   let items;
   const passes = "Backstage passes to a TAFKAL80ETC concert";
 
-  it("the product is returned", function () {
+  it("the correct product is returned", function () {
     gildedRose = new Shop([new Item(passes, 0, 0)]);
     items = gildedRose.updateQuality();
-    expect(items[0].name).to.equal(passes);
+    expect(items[0].name).to.equal("Backstage passes to a TAFKAL80ETC concert");
   });
 
   describe("Quality is updated", function () {
+
+    it("when sellIn is negative, the quality is 0", function () {
+      gildedRose = new Shop([new Item(passes, -15, 25)]);
+      items = gildedRose.updateQuality();
+      expect(items[0].quality).to.equal(0);
+    });
+
+    it("when sellIn is 0, and quality is 7, the quality goes to 0", function () {
+      const gildedRose = new Shop([new Item(passes, 0, 7)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).to.equal(0);
+    });
+
+    it("when sellIn is 5, and quality is 49, the quality goes to 50", function () {
+      const gildedRose = new Shop([new Item(passes, 5, 49)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).to.equal(50);
+    });
+
+    it("When sellIn is 6, and quality is 6. the quality goes to 8", function () {
+      gildedRose = new Shop([new Item(passes, 6, 6)]);
+      items = gildedRose.updateQuality();
+      expect(items[0].quality).to.equal(8);
+    });
 
     it("When quality is 0, it goes to 2", function () {
       gildedRose = new Shop([new Item(passes, 7, 0)]);
       items = gildedRose.updateQuality();
       expect(items[0].quality).to.equal(2);
-      expect(items[0].quality).not.to.equal(1);
     });
 
     it("When quality is max, it stays at max", function () {
@@ -134,59 +151,41 @@ describe("Backstage passes to a TAFKAL80ETC concert", function () {
       expect(items[0].quality).to.equal(50);
     });
 
-    it("When quality is 49, it goes to 50", function () {
+    it("When quality is 40, it goes to 43", function () {
       gildedRose = new Shop([new Item(passes, 5, 40)]);
       items = gildedRose.updateQuality();
       expect(items[0].quality).to.equal(43);
     });
+
+
   });
 
   describe("SellIn is updated", function () {
 
-    it("When sellIn is 7", function () {
-      gildedRose = new Shop([new Item(passes, 7, 0)]);
+    it("When sellIn goes to 0 (= after the concert has taken place), quality goes to 0", function () {
+      gildedRose = new Shop([new Item(passes, 1, 25)]);
       items = gildedRose.updateQuality();
-      expect(items[0].sellIn).to.equal(6);
+      expect(items[0].sellIn).to.equal(0);
     });
 
-    it("When sellIn is 0", function () {
+    it("When sellIn is 0, quality goes negative", function () {
       gildedRose = new Shop([new Item(passes, 0, 0)]);
       items = gildedRose.updateQuality();
       expect(items[0].sellIn).to.equal(-1);
     });
 
-  });
+    it("When sellIn is 7, quality goes to 6", function () {
+      gildedRose = new Shop([new Item(passes, 7, 0)]);
+      items = gildedRose.updateQuality();
+      expect(items[0].sellIn).to.equal(6);
+    });
 
-  it("quality goes to 0 after the concert has taken place", function () {
-    gildedRose = new Shop([new Item(passes, 1, 25)]);
-    items = gildedRose.updateQuality();
-    expect(items[0].sellIn).to.equal(0);
-  });
 
-  it("when sellIn is negative, the quality is 0", function () {
-    gildedRose = new Shop([new Item(passes, -15, 25)]);
-    items = gildedRose.updateQuality();
-    expect(items[0].quality).to.equal(0);
+
   });
 
 
-  it("when sellIn is 0, and quality is 7, the quality goes to 0", function () {
-    const gildedRose = new Shop([new Item(passes, 0, 7)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].quality).to.equal(0);
-  });
 
-  it("when sellIn is 5, and quality is 49, the quality goes to 50", function () {
-    const gildedRose = new Shop([new Item(passes, 5, 49)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].quality).to.equal(50);
-  });
-
-  it("When sellIn is 6, and quality is 6. the quality goes to 8", function () {
-    gildedRose = new Shop([new Item(passes, 6, 6)]);
-    items = gildedRose.updateQuality();
-    expect(items[0].quality).to.equal(8);
-  });
 
 
   it("When sellIn is 11, and quality is 6. the quality goes to 7", function () {
