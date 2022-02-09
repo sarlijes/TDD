38e4aebfa37942ed27dc8ca1c532405d32787d85
Jessica Sarlin <jessica.sarlin@gmail.com>
2022-02-09 16:15:44 +0200

    refactor to use beforeEach

--- tdd-mooc-game-of-life/test/utils.test.mjs
+++ tdd-mooc-game-of-life/test/utils.test.mjs
@@ -3,11 +3,18 @@ import { isValid } from "../src/utils.mjs";
 
 describe("Check cell validity", () => {
 
-  it("Valid cell", () => {
-    const arr = [];
+  const arr = [];
+
+  beforeEach(() => {
     arr.push([0, 0, 0]);
     arr.push([0, 0, 0]);
     arr.push([0, 0, 0]);
+  });
+
+
+  it("Cell is valid when both coordinates are 0", () => {
+
+
 
     expect(isValid(arr, 0, 0)).to.be.true;
   });
